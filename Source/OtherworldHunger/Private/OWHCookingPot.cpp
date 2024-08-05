ed44e4360da493b31c7ac7c99acc9366e4829cfa
Huda <huda.rasmey@gmail.com>
2024-08-05 14:05:26 +0300

    simple cut scene

--- Source/OtherworldHunger/Private/OWHCookingPot.cpp
+++ Source/OtherworldHunger/Private/OWHCookingPot.cpp
@@ -2,4 +2,17 @@
 
 
 #include "OWHCookingPot.h"
+#include "OWHCharacter.h"
 
+AActor* AOWHCookingPot::Interact_Implementation(APawn* InstigatorPawn)
+{
+	if (InstigatorPawn == nullptr) { return nullptr; }
+
+	if (AOWHCharacter* OwnerCharacter = Cast<AOWHCharacter>(InstigatorPawn))
+	{
+		OwnerCharacter->ShowNotification("Press C to cook", ENotificationType::EWarning);
+		return this;
+	}
+
+	return nullptr;
+}
--- Source/OtherworldHunger/Public/OWHCookingPot.h
+++ Source/OtherworldHunger/Public/OWHCookingPot.h
@@ -4,14 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "OWHInteractableInterface.h"
 #include "OWHCookingPot.generated.h"
 
 UCLASS()
-class OTHERWORLDHUNGER_API AOWHCookingPot : public AActor
+class OTHERWORLDHUNGER_API AOWHCookingPot : public AActor, public IOWHInteractableInterface
 {
 	GENERATED_BODY()
 
 public:
 	UFUNCTION(BlueprintImplementableEvent)
 	void Cook(bool StartCooking);
+
+protected:
+	virtual AActor* Interact_Implementation(APawn* InstigatorPawn) override;
 };
